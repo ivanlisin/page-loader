d1daa85678148a30625a30b0726aeda06fc2c750
Ivan Lisin <ivanlisin.tech@gmail.com>
2022-01-02 16:58:02 +0700

    remove axios

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,29 @@
 // @ts-check
 
-import axios from 'axios';
+import http from 'http';
+import https from 'https';
 import path from 'path';
 import Listr from 'listr';
 import { promises as fs } from 'fs';
 import { makeFileNameByUrl, makeDirNameByUrl, makeFilePathByUrl } from './name.js';
 
-const load = (href) => {
-  const options = { responseType: 'arraybuffer' };
-  return axios.get(href, options).then(({ data }) => data);
-};
+const fetch = (url) => new Promise((resolve, reject) => {
+  const protocol = url.startsWith('https') ? https : http;
+  protocol.get(url, (res) => {
+    if (res.statusCode !== 200) {
+      const { statusCode, statusMessage } = res;
+      reject(new Error(`Status Code: ${statusCode} | ${statusMessage}`));
+    }
+    res.setEncoding('utf8');
+    const buffer = [];
+    res.on('data', (chunk) => buffer.push(chunk));
+    res.on('end', () => resolve(buffer.join()));
+  });
+});
 
 const save = (filepath, data) => fs.writeFile(filepath, data, 'utf-8');
 
-export const loadIndex = (href) => load(href).then((data) => data.toString());
+export const loadIndex = (href) => fetch(href).then((data) => data.toString());
 
 export const downloadAssets = (url, outputDir, assets) => {
   const hasAssets = assets.length !== 0;
@@ -28,7 +38,7 @@ export const downloadAssets = (url, outputDir, assets) => {
     title: makeFileNameByUrl(url, asset),
     task: () => {
       const { href } = new URL(asset, url);
-      return load(href)
+      return fetch(href)
         .then((data) => {
           const filepath = makeFilePathByUrl(outputDir, url, asset);
           return save(filepath, data);
